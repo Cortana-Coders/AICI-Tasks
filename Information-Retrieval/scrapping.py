400d292742b31781e16a2281fe767961f325ff32
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2024-10-15 01:02:10 +0700

    move some files to new folders

--- Information-Retrieval/scrapping.py
+++ Information-Retrieval/scrapping.py
@@ -13,7 +13,7 @@ data_json = {
 }
 filename = 'superindo_locations.json'
 
-url = 'https://www.superindo.co.id/hubungi-kami/lokasi-superindo'
+url = 'https://www.superindo.co.id/hubungi-kami/lokasi-superindo/'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'    
