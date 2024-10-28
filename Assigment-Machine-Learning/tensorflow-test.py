5e6fb9d4ef13ce40af85f358af6d5e125cfdfb4f
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2024-10-28 14:20:13 +0700

    Remove unused code in tensorflow-test.py

--- Assigment-Machine-Learning/tensorflow-test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-%matplotlib inline
-
-df = pd.read_csv('Titanic_Train.csv')
-print(df.shape)
-df.head()
